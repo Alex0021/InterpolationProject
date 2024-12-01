de46ad285e0e4e99f6fdac2fb8f8e3b3c7181bcb
AlexWSL <alexandre-1.hebert@outlook.com>
2024-12-01 22:40:56 +0100

    Class and methods definition for the barycentric interpolator

---
+++ include/barycentric_interpolator.hpp
@@ -0,0 +1,55 @@
+#include "Eigen/Core"
+#include "polynomial_interpolator.hpp"
+
+/**
+ * @brief Class implementing the barycentric interpolation scheme
+ *
+ * @tparam T datapoints type (int, float, double)
+ */
+template <typename T>
+class BarycentricInterpolator: public PolynomialInterpolator<T>
+{
+    private:
+        Eigen::MatrixX<T> weights;
+
+        /**
+         * @brief Compute the weight i,j for the multidemnsional case (m>1)
+         * @param i row index for the weight matrix
+         * @param j column (dimension) index for the weight matrix (if m>1)
+         * @return calculated weight for x_ij
+         */
+        T barycentric_weight(unsigned int i, unsigned int j);
+
+        /** @brief Compute the weights defined as follow (2D):
+         * \f[
+         *      w_i = \left[ \prod_{j,j \neq i}^n (x_i-x_j) \right]^{-1}
+         * \f]
+         * @param i row index for the weight matrix
+         * @return calculated weight for x_ij
+         */
+        T barycentric_weight(unsigned int i);
+
+    public:
+
+        /**
+         * @brief Constructor for the BarycentricInterpolator object
+         */
+        BarycentricInterpolator();
+
+        /**
+         * @brief  Adds new data (N datapoints) to the interpolator.
+         * 
+         *  Recomputes only the necessary weights
+         * 
+         * @param X new datapoints to be added
+         */
+        void add_data(Eigen::MatrixX<T> X);
+
+        void fit(const Eigen::MatrixX<T>& X, unsigned int dim) override;
+
+        void fit(const Eigen::MatrixX<T>& X, const Eigen::VectorX<T>& y) override;
+
+        Eigen::VectorX<T> operator()(const Eigen::MatrixX<T>& X) override;
+
+        BarycentricInterpolator<T>& operator<<(Eigen::VectorX<T> x);
+};
---
+++ src/barycentric_interpolator.cpp
@@ -0,0 +1,33 @@
+#include "barycentric_interpolator.hpp"
+#include "exception"
+
+template <typename T>
+BarycentricInterpolator<T>::BarycentricInterpolator() {}
+
+template <typename T>
+T BarycentricInterpolator<T>::barycentric_weight(unsigned int i, unsigned int j) 
+{
+    throw std::runtime_error("FUNCTION NOT YET IMPLEMENTED ==> barycentric_weight");
+}
+
+template <typename T>
+void BarycentricInterpolator<T>::add_data(Eigen::MatrixX<T> X)
+{
+    throw std::runtime_error("FUNCTION NOT YET IMPLEMENTED ==> barycentric_weight");
+}
+
+template <typename T>
+void BarycentricInterpolator<T>::fit(const Eigen::MatrixX<T>& X, unsigned int dim) {}
+
+template <typename T>
+void BarycentricInterpolator<T>::fit(const Eigen::MatrixX<T>& X, const Eigen::VectorX<T>& y) {}
+
+template <typename T>
+Eigen::VectorX<T> BarycentricInterpolator<T>::operator()(const Eigen::MatrixX<T>& X) {}
+
+template <typename T>
+BarycentricInterpolator<T>& BarycentricInterpolator<T>::operator<<(Eigen::VectorX<T> x) {}
+
+template class BarycentricInterpolator<int>;
+template class BarycentricInterpolator<float>;
+template class BarycentricInterpolator<double>;
