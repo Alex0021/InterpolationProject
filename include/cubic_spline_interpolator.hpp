5af141268d0b17e7de0b17384b88c280fab25ad4
Max Brodeur <maxbrod21@gmail.com>
2024-12-11 20:41:14 +0100

    Cubic spline interpolator implementation

--- include/cubic_spline_interpolator.hpp
+++ include/cubic_spline_interpolator.hpp
@@ -1,10 +1,11 @@
 #ifndef __CUBIC_SPLINE_INTERPOLATOR_INCLUDE
 #define __CUBIC_SPLINE_INTERPOLATOR_INCLUDE
 
-#include "interpolator.hpp"
+#include "polynomial_interpolator.hpp"
+#include <Eigen/LU>
 
 template <typename T>
-class CubicSplineInterpolator: public Interpolator<T>
+class CubicSplineInterpolator: public PolynomialInterpolator<T>
 {
     public:
         /**
@@ -25,7 +26,11 @@ class CubicSplineInterpolator: public Interpolator<T>
 
         Eigen::MatrixX<T> X_data;
 
-        Eigen::VectorXd coefficients;
+        Eigen::MatrixX4<T> coefficients; // Coefficients of the cubic spline (a,b,c,d)
+
+        void _apply_boundary_conditions(Eigen::MatrixX<T> &A, Eigen::VectorX<T> &b);
+
+        int _get_index(T x);
 
     public:
 
@@ -35,11 +40,11 @@ class CubicSplineInterpolator: public Interpolator<T>
 
         void fit(const Eigen::MatrixX<T>& X, unsigned int dim_idx) override;
 
-        void fit(const Eigen::MatrixX<T>& X, const Eigen::VectorX<T>& y) override;
-
-        void fit(const Eigen::MatrixX<T>& X, const Eigen::VectorX<T>& y, BoundaryConstraint boundary);
+        void fit(const Eigen::VectorX<T>& X, const Eigen::VectorX<T>& y);
 
         Eigen::VectorX<T> operator()(const Eigen::MatrixX<T>& X) override;
+
+        T operator()(T x) override;
 
 };
 
