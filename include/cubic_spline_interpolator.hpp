9f7cce8ed4a55172e2e2556ba71fb90468cb18e7
Max Brodeur <maxbrod21@gmail.com>
2024-12-12 15:31:11 +0100

    documentation

--- include/cubic_spline_interpolator.hpp
+++ include/cubic_spline_interpolator.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file cubic_spline_interpolator.hpp
+ * @author Max Brodeur (maxbrod21@gmail.com)
+ * @brief A class for cubic spline interpolation
+ * @version 0.1
+ * @date 2024-12-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef __CUBIC_SPLINE_INTERPOLATOR_INCLUDE
 #define __CUBIC_SPLINE_INTERPOLATOR_INCLUDE
 
@@ -6,7 +17,11 @@
 #include <iostream>
 #include <Eigen/LU>
 
-
+/**
+ * @brief Templated class for cubic spline interpolation
+ * 
+ * @tparam T 
+ */
 template <typename T>
 class CubicSplineInterpolator: public PolynomialInterpolator<T>
 {
@@ -22,38 +37,113 @@ class CubicSplineInterpolator: public PolynomialInterpolator<T>
     
     private:
         /**
-         * @brief Boundary condition of the current spline instance
+         * @brief Boundary condition to apply to the spline interpolation
          */
         BoundaryConstraint boundary_constraint;
 
-        Eigen::MatrixX<T> X_data;
+        /**
+         * @brief Coefficients of the cubic spline interpolation 
+         * The cubic polynomial for the i-th segment represented as:
+         * \f[
+         * S_i(x) = a_i + b_i (x - x_i) + c_i (x - x_i)^2 + d_i (x - x_i)^3
+         * \f]
+         * where a_i, b_i, c_i, and d_i are the coefficients for the i-th cubic segment.
+         */
+        Eigen::MatrixX4<T> coefficients; 
 
-        Eigen::MatrixX4<T> coefficients; // Coefficients of the cubic spline (a,b,c,d)
-
+        /**
+         * @brief The values to clamp the spline to if the boundary condition is set to CLAMPED
+         */
         Eigen::Vector2<T> clamped_values;
 
+        /**
+         * @brief Private function for applying the boundary conditions for the trilinear system matrix A and the vector b. 
+         * Ex: NATURAL boundary conditions impose A_{11}=A_{nn}=1 and b_1=b_n=0
+         * 
+         * @param A : The trilinear system matrix
+         * @param b : The trilinear system vector
+         * @param f : Newton's divided differences `f[i] = (y[i+1] - y[i]) / h[i]\`
+         * @param h : The datapoint difference vector  `h[i] = x[i+1] - x[i]`
+         */
         void _apply_boundary_conditions(Eigen::MatrixX<T> &A, Eigen::VectorX<T> &b, Eigen::VectorX<T> &f, Eigen::VectorX<T> &h);
 
+        /**
+         * @brief A private method to identify the index of the segment (or 'bin') that contains the value x.
+         * 
+         * @param x : The value to find the segment for
+         * @return int : The index of the bin
+         */
         int _get_index(T x);
 
     public:
 
+        /**
+         * @brief Construct a new Cubic Spline Interpolator object. Defaults to NATURAL boundary conditions.
+         * 
+         */
         CubicSplineInterpolator();
 
+        /**
+         * @brief Construct a new Cubic Spline Interpolator object with the specified boundary conditions
+         * 
+         * @param boundary : boundary condition to apply to the spline
+         */
         CubicSplineInterpolator(BoundaryConstraint boundary);
 
+        /**
+         * @brief Construct a new Cubic Spline Interpolator object for clamped boundary conditions with the specified clamped values
+         * 
+         * @param boundary : must be set to CLAMPED to use clamped values
+         * @param clamped_values : The values to clamp the spline to
+         */
         CubicSplineInterpolator(BoundaryConstraint boundary, const Eigen::Vector2<T>& clamped_values);
 
+        /**
+         * @brief Utility wrapper over the fit method to fit the data to the specified dimension index
+         * 
+         * @param X 
+         * @param dim_idx 
+         */
         void fit(const Eigen::MatrixX<T>& X, unsigned int dim_idx) override;
 
+        /**
+         * @brief Fit the data to the cubic spline interpolator using the specified X and y data. 
+         * Computes the coefficients for the cubic spline by solving a trilinear system of equations.
+         * 
+         * @param X : The datapoints to fit the spline to 
+         * @param y : The values of the datapoints to fit the spline to
+         */
         void fit(const Eigen::VectorX<T>& X, const Eigen::VectorX<T>& y);
 
+        /**
+         * @brief Query the cubic spline interpolator for the interpolated values at the specified datapoints X
+         * 
+         * @param X : The datapoints to query the interpolator for
+         * @return Eigen::VectorX<T> : The interpolated values at the specified datapoints
+         */
         Eigen::VectorX<T> operator()(const Eigen::MatrixX<T>& X) override;
 
+        /**
+         * @brief Query the cubic spline interpolator for the interpolated value at the specified datapoint x
+         * 
+         * @param x : The datapoint to query the interpolator for
+         * @return T : The interpolated value at the specified datapoint
+         */
         T operator()(T x) override;
 
+        /**
+         * @brief Set the clamped values object for CLAMPED boundary conditions
+         * 
+         * @param clamped_values 
+         */
         void set_clamped_values(const Eigen::Vector2<T>& clamped_values);
         
+        /**
+         * @brief Set the clamped values object for CLAMPED boundary conditions
+         * 
+         * @param low 
+         * @param high 
+         */
         void set_clamped_values(T low, T high);
 
 };
--- include/datagen.hpp
+++ include/datagen.hpp
@@ -1,3 +1,15 @@
+/**
+ * @file datagen.hpp
+ * @author Max Brodeur (maxbrod21@gmail.com)
+ * @brief A utility class for generating and writing data to test interpolation algorithms. 
+ * Contains methods for generating data of different sources, such as random, polynomial, and damped cosine functions. 
+ * 
+ * @version 0.1
+ * @date 2024-12-12
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #pragma once
 
 #include <Eigen/Core>
@@ -10,40 +22,118 @@
 // #ifndef DATAGEN_HPP 
 // #define DATAGEN_HPP
 
+/**
+ * @brief A utility class for generating and writing data to test interpolation algorithms.
+ * 
+ * @tparam T 
+ */
 template <typename T>
 struct datagen {
-    /**
-     * @brief 
-     * 
-     * @tparam T 
-     * @param path write the data to this file
-     * @param n number of points
-     * @param m number of features
-     * @param degree degree of the polynomial
-     */
 
+    /**
+     * @brief Point generation paradigms to populate the domain of the data.
+     * 
+     */
     enum class PointGeneration {
         RANDOM_UNIFORM,
         CHEBYSHEV,
         UNIFORM,
     };
 
+    /**
+     * @brief Write the data to a file at the specified path. It is space separated and newline delimited. The first rows start with a '#' and contain metadata, such as the dimensions of the data.
+     * TODO: be more specific about the format of the data, once we have chosen the convention. 
+     * 
+     * @param path : The path to write the data to
+     * @param data : The data to write
+     */
     static void write(const std::filesystem::path path, const Eigen::MatrixX<T> &data);
 
+    /**
+     * @brief Generate random data of size n \times m and write it to the specified path.
+     * 
+     * @param path : The path to write the data to
+     * @param n : The number of rows
+     * @param m : The number of columns
+     */
     static void random_data(std::filesystem::path path, int n, int m);
 
+    /**
+     * @brief The multi-dimensional polynomial counterpart to the polynomial method. **Unsuppported**
+     * 
+     * @param data 
+     * @param coeffs 
+     * @param range 
+     * @param pg 
+     */
     static void polynomial(Eigen::MatrixX<T> &data, Eigen::VectorX<T> &coeffs, std::vector<std::pair<T, T>> range, PointGeneration pg);
-    
+
+    /**
+     * @brief Generate a polynomial function of the specified coefficients and write it to the specified path. 
+     * The degree of the polynomial is determined by the size of the coefficients vector.
+     * E.g. for a polynomial of degree n, the coefficients vector should have n+1 elements.:
+     * \f[
+     * f(x) = a_0 + a_1 x + a_2 x^2 + \ldots + a_n x^n
+     * \f]
+     * 
+     * @param data : The data to write the polynomial to. So both the domain (data.col(0)) and the range (data.col(1)) of the polynomial.
+     * @param coeffs : The coefficients of the polynomial
+     * @param range : The range of the data to generate
+     * @param pg : The point generation paradigm to use
+     */    
     static void polynomial(Eigen::MatrixX2<T> &data, Eigen::VectorX<T> &coeffs, std::pair<T, T> range, PointGeneration pg);
     
+    /**
+     * @brief Helper function to generate a polynomial function of the specified coefficients and populate the data vector with the values.
+     * 
+     * @param data : The vector to populate with the polynomial values
+     * @param x : The domain of the polynomial
+     * @param coeffs : The coefficients of the polynomial
+     */
     static void polynomial(Eigen::VectorX<T> &data, Eigen::VectorX<T> &x, Eigen::VectorX<T> &coeffs);
-    
-    static void chebyshev_polynomial(Eigen::VectorX<T> &data, int d, std::pair<T, T> range, PointGeneration pg);
 
+    /**
+     * @brief Generate a set of points according to the specified paradigm. 
+     * 
+     * @param x : The vector to populate with the points
+     * @param range : The range of the data to generate
+     * @param pg : The point generation paradigm to use
+     */
     static void generate_points(Eigen::VectorX<T> &x, std::pair<T, T> range, PointGeneration pg);
 
+    /**
+     * @brief Implementation of the Chebyshev points generation paradigm.
+     * The points are generated according to 
+     * \f[
+     * x_i = \frac{1}{2} \left( a + b \right) + \frac{1}{2} \left( b - a \right) \cos \left( \frac{2i + 1}{2n} \pi \right)
+     * \f]
+     * where a and b are the bounds of the range and n is the number of points to generate.
+     * 
+     * Note: The points are generated in the range (a, b) but the first and last points are not included in the range, therefore creating
+     * a risk of extrapolation if the interpolation algorithm is not robust to it.
+     * 
+     * @param x : The vector to populate with the points
+     * @param range : The range of the data to generate (a, b)
+     */
     static void chebyshev_points(Eigen::VectorX<T> &x, std::pair<T, T> range);
 
+    /**
+     * @brief Generate a damped cosine function according to the specified parameters and write it to the specified path.
+     * The damped cosine function is defined as:
+     * \f[
+     * f(x) = a e^{-bx^2} \cos(cx)
+     * \f]
+     * 
+     * The parameters have the following effect:
+     * - a : The amplitude of the cosine wave
+     * - b : The damping factor (the rate at which the amplitude decreases)
+     * - c : The frequency of the cosine wave
+     * 
+     * @param data : The data to write the damped cosine function to. So both the domain (data.col(0)) and the range (data.col(1)) of the function.
+     * @param params : The parameters of the damped cosine function
+     * @param range : The range of the data to generate
+     * @param pg : The point generation paradigm to use
+     */
     static void damped_cosine(Eigen::MatrixX2<T> &data, Eigen::VectorX<T> &params, std::pair<T, T> range, PointGeneration pg);
 
 };
